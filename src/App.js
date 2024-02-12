37e28ffb55dc0141886b82ef073a9d31492b1656
Zahid Afsar <zahidtempz@icloud.com>
2024-02-12 10:02:02 -0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+// useEffect hook that updates the state of name by calling the function setName
+const [name, setName] = useState('Jacoozi');
+
+const handleClick = () => {
+  setName('Evil Jacoozi');
+}
+
+// every time the componenent state is rendered the useEffect Hook is ran
+// This hook runs as soon as the component is rendered
+useEffect(() => {
+
+  console.log("useEffect ran");
+  
+},[name]) //Dependancy array - what ever in our depenancy array will effect whehter or ot our useEffect will run
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   {/* Calling our state variable in the JSX using { } */}
+   <h1>{name}</h1>
+   <Button variant="dark" onClick={() => handleClick()} >Dark</Button>
+   </>
   );
 }
 
